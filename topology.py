15b3678f3d66de220d538ebc49eb5a29dfe855ab
Leticia Sales <lgsm15@inf.ufpr.br>
2018-06-18 22:02:11 -0300

    .

--- topology.py
+++ topology.py
@@ -4,14 +4,14 @@ from mininet.topo import Topo
 class MyTopo(Topo):
     def __init__(self, n = 4):
         Topo.__init__(self)
-        first = null
+        first = None
         for x in random.random:
             first = switch
             switch = self.addSwitch('s%s' % (x))
             for h in range(n):
                 host = self.addHost('h%s' % (h + 1))
                 self.addLink(host, switch)
-            if first != null
+            if first
                 self.addLink(first, switch)
 
 topos = { 'mytopo': (lambda: MyTopo()) }
