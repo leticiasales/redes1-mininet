30575611e5d3d607c1256c2a61ab7230e8d24a44
Leticia Sales <lgsm15@inf.ufpr.br>
2018-06-18 22:50:58 -0300

    .

--- topology.py
+++ topology.py
@@ -10,7 +10,7 @@ class MyTopo(Topo):
             first = switch
             switch = self.addSwitch('s%s' % (x))
             for h in range(randint(0, 5)):
-                host = self.addHost('h%s' % (randint(1, magic*2))
+                host = self.addHost('h%s' % (randint(1, magic*2)))
                 self.addLink(host, switch)
             if first:
                 linkopts = dict(bw=magic, delay='5ms', loss=10-magic, max_queue_size=1000, use_htb=True)
