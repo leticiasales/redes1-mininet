e322183b6b4381c5c6bdb5d903c9949c12c4e6ba
Leticia Sales <lgsm15@inf.ufpr.br>
2018-06-18 22:11:58 -0300

    .

--- topology.py
+++ topology.py
@@ -2,13 +2,13 @@ from random import randint
 from mininet.topo import Topo
 
 class MyTopo(Topo):
-    def __init__(self, n = 4):
+    def __init__(self):
         Topo.__init__(self)
-        first = None
-        for x in range(randint(0, 20)):
+        switch = None
+        for x in range(randint(0, 10)):
             first = switch
             switch = self.addSwitch('s%s' % (x))
-            for h in range(n):
+            for h in range(randint(0, 20)):
                 host = self.addHost('h%s' % (h + 1))
                 self.addLink(host, switch)
             if first:
